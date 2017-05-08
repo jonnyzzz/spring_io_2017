98742f17861ffb641be5c4a5c2fb128cfa579bc3
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2017-05-08 14:50:01 +0200

    move sources

--- src/main/java/plugin/extensions/v1/api.kt
+++ src/main/java/plugin/extensions/v1/api.kt
@@ -1,8 +1,5 @@
 package plugin.extensions.v1
 
-import org.springframework.context.ApplicationContext
-import org.springframework.context.support.ClassPathXmlApplicationContext
-
 interface Extension {
   //Extension API
 }
@@ -11,31 +8,6 @@ interface ExtensionRegistry {
 }
 interface ExtensionHolder {
   val extensions: List<Exception>
-}
-
-
-
-fun loadPlugins(pluginId: String, parent: ApplicationContext) {
-  ClassPathXmlApplicationContext(
-          arrayOf("classpath*:/META-INF/$pluginId/config*.xml"),
-          true,
-          parent)
-
-
-
-}
-
-fun main(args: Array<String>) {
-
-}
-
-
-
-fun usesAll() {
-  ExtensionRegistry::class
-  ExtensionHolder::class
-  ExtensionRegistry::register
-  ExtensionHolder::extensions
 }
 
 
--- src/main/java/plugin/extensions/v1/loader.kt
+++ src/main/java/plugin/extensions/v1/loader.kt
@@ -1,2 +1,61 @@
-package plugin.extensions.v1
+package plugin.extensions.v1.core
+
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import org.springframework.context.support.ClassPathXmlApplicationContext
+import org.springframework.stereotype.Component
+import plugin.extensions.v1.ExtensionHolder
+import plugin.extensions.v1.ExtensionRegistry
+
+fun main(args: Array<String>) = RootContext().start()
+
+class RootContext {
+  fun start() {
+    val root = AnnotationConfigApplicationContext()
+    root.scan(RootContext::class.java.`package`.name)
+    root.refresh()
+    root.start()
+
+    println()
+    println("RootContext: Start Completed")
+    //wait
+    readLine()
+  }
+}
+
+@Component
+class CoreComponent {
+  init {
+    println("I'm core component")
+  }
+}
+
+@Component
+class PrivateComponent {
+  init {
+    println("I'm private component")
+  }
+}
+
+
+
+fun loadPlugins(pluginId: String, parent: ApplicationContext) {
+  ClassPathXmlApplicationContext(
+          arrayOf("classpath*:/META-INF/$pluginId/config*.xml"),
+          true,
+          parent)
+
+
+
+}
+
+
+
+fun usesAll() {
+  ExtensionRegistry::class
+  ExtensionHolder::class
+  ExtensionRegistry::register
+  ExtensionHolder::extensions
+}
+
 
