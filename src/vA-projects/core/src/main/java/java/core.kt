67d81575ba1d8790c36dd0f56670a11ab4261b20
Eugene Petrenko <eugene.petrenko@gmail.com>
2017-05-11 23:01:20 +0200

    copy-paste example

---
+++ src/vA-projects/core/src/main/java/java/core.kt
@@ -0,0 +1,14 @@
+package plugin.extensions.core
+
+import org.springframework.stereotype.Component
+import plugin.extensions.CoreService
+
+@Component
+class AdHocCoreService : CoreService {
+  override fun `loren ipsum`() {
+    println("CoreService: Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
+            "\r\n             Donec a ullamcorper ex. Praesent tincidunt sit amet dolor a " +
+            "\r\n             sodales. Sed nec feugiat mi. Vivamus purus diam, interdum " +
+            "\r\n             eleifend risus non, pharetra laoreet est. Mauris eu dolor... ")
+  }
+}
