3c5a264fec1dde3abc9ef80c5211cebc72368bbf
Eugene Petrenko <eugene.petrenko@gmail.com>
2017-05-11 15:30:09 +0200

    rename

---
+++ src/v7-guava-collision/api/src/main/java/java/api.kt
@@ -0,0 +1,19 @@
+package plugin.extensions
+
+annotation class ProvidedByPlugin
+annotation class ProvidedByCore
+
+@ProvidedByPlugin
+interface Extension {
+  //Extension API
+}
+
+@ProvidedByCore
+interface ExtensionHolder {
+  val extensions: List<Extension>
+}
+
+@ProvidedByCore
+interface CoreService {
+  fun `loren ipsum`()
+}
