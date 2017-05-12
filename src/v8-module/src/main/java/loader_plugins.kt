3f2b259db21c48b4b36e96b75da95fe32493922c
Eugene Petrenko <eugene.petrenko@gmail.com>
2017-05-12 23:23:39 +0200

    include fix for collision

--- src/v8-module/src/main/java/loader_plugins.kt
+++ src/v8-module/src/main/java/loader_plugins.kt
@@ -1,18 +1,28 @@
 package plugin.extensions.core
 
+import org.springframework.beans.factory.BeanNameAware
 import org.springframework.beans.factory.InitializingBean
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.config.BeanDefinitionHolder
+import org.springframework.beans.factory.config.DependencyDescriptor
+import org.springframework.beans.factory.support.DefaultListableBeanFactory
+import org.springframework.beans.factory.support.SimpleAutowireCandidateResolver
 import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import plugin.extensions.Extension
 
 abstract class PluginLoader(
         val name: String
-) : InitializingBean {
+) : InitializingBean, BeanNameAware {
   @Autowired lateinit var registry: ExtensionRegistry
   @Autowired lateinit var parentContext : ApplicationContext
 
   var context = null as AnnotationConfigApplicationContext?
+  var beanFactoryName = null as String?
+
+  override fun setBeanName(name: String?) {
+    beanFactoryName = name!!
+  }
 
   override fun afterPropertiesSet() {
     println("PluginLoader: loading plugin $name...")
@@ -23,6 +33,15 @@ abstract class PluginLoader(
     context.parent = parentContext
     context.displayName = "plugin: $name"
     context.scan(Extension::class.java.`package`.name + "." + name)
+
+    (context.beanFactory as DefaultListableBeanFactory).autowireCandidateResolver =
+            object : SimpleAutowireCandidateResolver() {
+              override fun isAutowireCandidate(bdHolder: BeanDefinitionHolder,
+                                               descriptor: DependencyDescriptor)
+                      = super.isAutowireCandidate(bdHolder, descriptor) &&
+                      beanFactoryName != bdHolder.beanDefinition.factoryBeanName
+            }
+    
     context.refresh()
 
     context.getBeansOfType(Extension::class.java)
