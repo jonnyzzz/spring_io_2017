597a281196353a80cb3010ffd07ce79fe6a6b49f
Eugene Petrenko <eugene.petrenko@gmail.com>
2017-05-15 13:41:00 +0200

    add jar hell problem

--- src/vA-projects/va-plugin-1/src/main/java/plugin_1.kt
+++ src/vA-projects/va-plugin-1/src/main/java/plugin_1.kt
@@ -1,5 +1,6 @@
 package plugin.extensions.plugin_1
 
+import com.google.common.collect.ImmutableList
 import org.springframework.stereotype.Component
 import plugin.extensions.ServiceFromThePlugin1
 
@@ -11,5 +12,24 @@ class ServiceFromThePlugin1Impl : ServiceFromThePlugin1 {
 
   override fun test() {
     println("Plugin 1 #test()")
+  }
+}
+
+
+@Component
+class PluginGuavaVersion {
+  init {
+    println()
+
+    val clazz = ImmutableList::class.java
+    val guava = clazz.getResource("/" + clazz.name.replace(".", "/") + ".class").toString()
+
+    when {
+      guava.contains("guava-21.0.jar") -> println("Plugin 1 Guava 21(!) URL " + guava)
+      guava.contains("guava-14.0.jar") -> println("Plugin 1 Guava 14    URL " + guava)
+      else ->                             println("Plugin 1 Guava ???   URL " + guava)
+    }
+
+    println()
   }
 }
--- src/vA-projects/va-plugin-2/src/main/java/plugin_2.kt
+++ src/vA-projects/va-plugin-2/src/main/java/plugin_2.kt
@@ -1,5 +1,6 @@
 package plugin.extensions.plugin_2
 
+import com.google.common.collect.ImmutableList
 import org.springframework.stereotype.Component
 import plugin.extensions.ServiceFromThePlugin2
 
@@ -11,5 +12,24 @@ class ServiceFromThePlugin2Impl : ServiceFromThePlugin2 {
 
   init {
     println("Plugin 2 Exported")
+  }
+}
+
+
+@Component
+class PluginGuavaVersion {
+  init {
+    println()
+
+    val clazz = ImmutableList::class.java
+    val guava = clazz.getResource("/" + clazz.name.replace(".", "/") + ".class").toString()
+
+    when {
+      guava.contains("guava-21.0.jar") -> println("Plugin 1 Guava 21(!) URL " + guava)
+      guava.contains("guava-14.0.jar") -> println("Plugin 1 Guava 14    URL " + guava)
+      else ->                             println("Plugin 1 Guava ???   URL " + guava)
+    }
+
+    println()
   }
 }
